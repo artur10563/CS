67a7451381b078918dcdbda4b98fff3d2fb61b31
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-10-15 22:59:47 +0300

    init Lab1_Blockchain

---
+++ LR1_BlockchainPrototype/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LR1_BlockchainPrototype;
+
+
+public sealed record Block();
+public sealed record Transaction(string sender, string recipient, decimal amount);
+
+public class BlockchainPrototype
+{
+    public readonly List<Block> blockchain = [];
+    public readonly List<Transaction> transactions = [];
+
+    public void CreateTransaction(Transaction transaction)
+    {
+        transactions.Add(transaction);
+        
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
