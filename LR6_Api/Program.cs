ff18c139f10cf215d3682a7bb57b8e4d1644ea9f
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-12-23 17:39:29 +0200

    LR6 - Dockerfile

--- LR6_Api/Program.cs
+++ LR6_Api/Program.cs
@@ -20,7 +20,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     .AddDefaultNodeIdSerializer()
     ;
 
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGraphQL();
 
